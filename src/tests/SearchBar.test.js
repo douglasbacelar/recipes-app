d21e2ca84a9a630a9c040b235d60d0461cde1a3a
KhallylReisFonseca <khallylreis@gmail.com>
2023-04-11 15:10:25 -0300

    Adiciona 100% de cobertura para a SearchBar!

--- src/tests/SearchBar.test.js
+++ src/tests/SearchBar.test.js
@@ -1,4 +1,4 @@
-/* // Cria teste para cobrir 90% do componente SearchBar.js!
+// Cria teste para cobrir 90% do componente SearchBar.js!
 
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
@@ -6,7 +6,7 @@ import { BrowserRouter, MemoryRouter } from 'react-router-dom';
 import userEvent from '@testing-library/user-event';
 import HeaderProvider from '../provider/HeaderProvider';
 // import LoginProvider from '../providers/LoginProvider';
-// import ApiProvider from '../providers/ApiProvider';
+import ApiProvider from '../provider/ApiProvider';
 import Meals from '../pages/Meals';
 import Drinks from '../pages/Drinks';
 import HeaderContext from '../context/HeaderContext';
@@ -39,9 +39,11 @@ describe('Testa o componente SearchBar.js', () => {
   test('testa se ao pesquisar pelo componente Meals com apenas uma letra pela opção first letter, o retorno da API é o correto!', async () => {
     // Arrange
     render(
-      <HeaderProvider>
-        <Meals />
-      </HeaderProvider>,
+      <ApiProvider>
+        <HeaderProvider>
+          <Meals />
+        </HeaderProvider>
+      </ApiProvider>,
       { wrapper: BrowserRouter },
     );
     // Act
@@ -64,9 +66,11 @@ describe('Testa o componente SearchBar.js', () => {
   test('testa se ao pesquisar pelo componente Meals por um nome, o retorno da API é o correto!', async () => {
     // Arrange
     render(
-      <HeaderProvider>
-        <Meals />
-      </HeaderProvider>,
+      <ApiProvider>
+        <HeaderProvider>
+          <Meals />
+        </HeaderProvider>
+      </ApiProvider>,
       { wrapper: BrowserRouter },
     );
     // Act
@@ -89,9 +93,11 @@ describe('Testa o componente SearchBar.js', () => {
   test('testa se ao pesquisar pelo componente Meals por um ingrediente, o retorno da API é o correto!', async () => {
     // Arrange
     render(
-      <HeaderProvider>
-        <Meals />
-      </HeaderProvider>,
+      <ApiProvider>
+        <HeaderProvider>
+          <Meals />
+        </HeaderProvider>
+      </ApiProvider>,
       { wrapper: BrowserRouter },
     );
     // Act
@@ -109,168 +115,6 @@ describe('Testa o componente SearchBar.js', () => {
       const recipeImg = screen.findByTestId(recipeP);
       expect(recipeImg).toBe('https://www.themealdb.com/images/media/meals/wwuqvt1487345467.jpg');
       expect(recipeTlt.innerHTML).toBe('Osso Buco alla Milanese');
-    });
-  });
-  test('testa se ao pesquisar pelo componente Drinks por um nome, o retorno da API é o correto!', async () => {
-    // Arrange
-    render(
-      <HeaderProvider>
-        <Drinks />
-      </HeaderProvider>,
-      { wrapper: BrowserRouter },
-    );
-    // Act
-    const searchButton = screen.getByTestId(search);
-    userEvent.click(searchButton);
-    const searchInput = screen.getByTestId(searchInputLint);
-    const searchButton2 = screen.getByTestId(execSearch);
-    const NameRadio = screen.getByTestId(nameRadio);
-    userEvent.click(NameRadio);
-    userEvent.type(searchInput, 'a1');
-    userEvent.click(searchButton2);
-    // Assert
-    waitFor(() => {
-      const recipeTlt = screen.findByTestId(recipeT);
-      const recipeImg = screen.findByTestId(recipeP);
-      expect(recipeImg).toBe('https://www.thecocktaildb.com/images/media/drink/2x8thr1504816928.jpg');
-      expect(recipeTlt.innerHTML).toBe('A1');
-    });
-  });
-  test('testa se ao pesquisar pelo componente Drinks por uma letra, o retorno da API é o correto!', async () => {
-    // Arrange
-    render(
-      <HeaderProvider>
-        <Drinks />
-      </HeaderProvider>,
-      { wrapper: BrowserRouter },
-    );
-    // Act
-    const searchButton = screen.getByTestId(search);
-    userEvent.click(searchButton);
-    const searchInput = screen.getByTestId(searchInputLint);
-    const searchButton2 = screen.getByTestId(execSearch);
-    const firstLetter = screen.getByTestId(letter);
-    userEvent.click(firstLetter);
-    userEvent.type(searchInput, '4');
-    userEvent.click(searchButton2);
-    // Assert
-    waitFor(() => {
-      const fristImg = screen.findByTestId(cardZero);
-      const fristCardName = screen.findByTestId(recipeZero);
-      expect(fristImg).toBe('https://www.thecocktaildb.com/images/media/drink/xtuyqv1472669026.jpg');
-      expect(fristCardName).toBe('410 Gone');
-    });
-  });
-  test('testa se ao pesquisar pelo componente Drinks por um nome, o retorno da API é o correto!', async () => {
-    // Arrange
-    render(
-      <HeaderProvider>
-        <Drinks />
-      </HeaderProvider>,
-      { wrapper: BrowserRouter },
-    );
-    // Act
-    const searchButton = screen.getByTestId(search);
-    userEvent.click(searchButton);
-    const searchInput = screen.getByTestId(searchInputLint);
-    const searchButton2 = screen.getByTestId(execSearch);
-    const NameRadio = screen.getByTestId(nameRadio);
-    userEvent.click(NameRadio);
-    userEvent.type(searchInput, '6');
-    userEvent.click(searchButton2);
-    // Assert
-    waitFor(() => {
-      const fristImg = screen.findByTestId(cardZero);
-      const fristCardName = screen.findByTestId(recipeZero);
-      expect(fristImg).toBe('https://www.thecocktaildb.com/images/media/drink/vqyxqx1472669095.jpg');
-      expect(fristCardName).toBe('69 Special');
-    });
-  });
-  test('testa se ao pesquisar pelo componente Drinks por um ingrediente, o retorno da API é o correto!', async () => {
-    // Arrange
-    render(
-      <HeaderProvider>
-        <Drinks />
-      </HeaderProvider>,
-      { wrapper: BrowserRouter },
-    );
-    // Act
-    const searchButton = screen.getByTestId(search);
-    userEvent.click(searchButton);
-    const searchInput = screen.getByTestId(searchInputLint);
-    const searchButton2 = screen.getByTestId(execSearch);
-    const ingRa = screen.getByTestId(ingRadio);
-    userEvent.click(ingRa);
-    userEvent.type(searchInput, 'Limeade');
-    userEvent.click(searchButton2);
-    // Assert
-    waitFor(() => {
-      const fristImg = screen.findByTestId(cardZero);
-      const fristCardName = screen.findByTestId(recipeZero);
-      expect(fristImg).toBe('https://www.thecocktaildb.com/images/media/drink/xwxyux1441254243.jpg');
-      expect(fristCardName).toBe('Vodka Fizz');
-    });
-  });
-  test('testa se ao pesquisar por duas letras exibe um alert', async () => {
-    // Arrange
-    render(
-      <HeaderProvider>
-        <Meals />
-      </HeaderProvider>,
-      { wrapper: BrowserRouter },
-    );
-    // Act
-    const searchButton = screen.getByTestId(search);
-    userEvent.click(searchButton);
-    const searchInput = screen.getByTestId(searchInputLint);
-    const searchButton2 = screen.getByTestId(execSearch);
-    const firstLetter = screen.getByTestId(letter);
-    userEvent.click(firstLetter);
-    userEvent.type(searchInput, 'Limeade');
-    userEvent.click(searchButton2);
-    // Assert
-    waitFor(() => {
-      expect(global.alert).toBeCalledWith('Your search must have only 1 (one) character');
-    });
-  });
-  test('testa se ao pesquisar sem inserir nada no input search exibe um alert', async () => {
-    // Arrange
-    render(
-      <HeaderProvider>
-        <Meals />
-      </HeaderProvider>,
-      { wrapper: BrowserRouter },
-    );
-    // Act
-    const searchButton = screen.getByTestId(search);
-    userEvent.click(searchButton);
-    const searchButton2 = screen.getByTestId(execSearch);
-    userEvent.click(searchButton2);
-    // Assert
-    waitFor(() => {
-      expect(global.alert).toBeCalledWith('Search term not entered');
-    });
-  });
-  test('testa se ao pesquisar por algum termo que não existe receitas é exibido um alert', async () => {
-    // Arrange
-    render(
-      <HeaderProvider>
-        <Meals />
-      </HeaderProvider>,
-      { wrapper: BrowserRouter },
-    );
-    // Act
-    const searchButton = screen.getByTestId(search);
-    userEvent.click(searchButton);
-    const searchInput = screen.getByTestId(searchInputLint);
-    const searchButton2 = screen.getByTestId(execSearch);
-    const NameRadio = screen.getByTestId(nameRadio);
-    userEvent.click(NameRadio);
-    userEvent.type(searchInput, 'aaaaaa');
-    userEvent.click(searchButton2);
-    // Assert
-    waitFor(() => {
-      expect(global.alert).toBeCalledWith('Sorry, we haven\'t found any recipes for these filters.');
     });
   });
   test('Testa se ao chamar fetchDrinksAPI com os parâmetros corretos a rota é /meals', async () => {
@@ -473,5 +317,180 @@ describe('Testa o componente SearchBar.js', () => {
       expect(mockHistoryPush).toHaveBeenCalledWith('/drinks/17222');
     });
   });
+  test('testa se ao pesquisar pelo componente Drinks por um nome, o retorno da API é o correto!', async () => {
+    // Arrange
+    render(
+      <ApiProvider>
+        <HeaderProvider>
+          <Drinks />
+        </HeaderProvider>
+      </ApiProvider>,
+      { wrapper: BrowserRouter },
+    );
+    // Act
+    const searchButton = screen.getByTestId(search);
+    userEvent.click(searchButton);
+    const searchInput = screen.getByTestId(searchInputLint);
+    const searchButton2 = screen.getByTestId(execSearch);
+    const NameRadio = screen.getByTestId(nameRadio);
+    userEvent.click(NameRadio);
+    userEvent.type(searchInput, 'a1');
+    userEvent.click(searchButton2);
+    // Assert
+    waitFor(() => {
+      const recipeTlt = screen.findByTestId(recipeT);
+      const recipeImg = screen.findByTestId(recipeP);
+      expect(recipeImg).toBe('https://www.thecocktaildb.com/images/media/drink/2x8thr1504816928.jpg');
+      expect(recipeTlt.innerHTML).toBe('A1');
+    });
+  });
+  test('testa se ao pesquisar pelo componente Drinks por uma letra, o retorno da API é o correto!', async () => {
+    // Arrange
+    render(
+      <ApiProvider>
+        <HeaderProvider>
+          <Drinks />
+        </HeaderProvider>
+      </ApiProvider>,
+      { wrapper: BrowserRouter },
+    );
+    // Act
+    const searchButton = screen.getByTestId(search);
+    userEvent.click(searchButton);
+    const searchInput = screen.getByTestId(searchInputLint);
+    const searchButton2 = screen.getByTestId(execSearch);
+    const firstLetter = screen.getByTestId(letter);
+    userEvent.click(firstLetter);
+    userEvent.type(searchInput, '4');
+    userEvent.click(searchButton2);
+    // Assert
+    waitFor(() => {
+      const fristImg = screen.findByTestId(cardZero);
+      const fristCardName = screen.findByTestId(recipeZero);
+      expect(fristImg).toBe('https://www.thecocktaildb.com/images/media/drink/xtuyqv1472669026.jpg');
+      expect(fristCardName).toBe('410 Gone');
+    });
+  });
+  test('testa se ao pesquisar pelo componente Drinks por um nome, o retorno da API é o correto!', async () => {
+    // Arrange
+    render(
+      <ApiProvider>
+        <HeaderProvider>
+          <Drinks />
+        </HeaderProvider>
+      </ApiProvider>,
+      { wrapper: BrowserRouter },
+    );
+    // Act
+    const searchButton = screen.getByTestId(search);
+    userEvent.click(searchButton);
+    const searchInput = screen.getByTestId(searchInputLint);
+    const searchButton2 = screen.getByTestId(execSearch);
+    const NameRadio = screen.getByTestId(nameRadio);
+    userEvent.click(NameRadio);
+    userEvent.type(searchInput, '6');
+    userEvent.click(searchButton2);
+    // Assert
+    waitFor(() => {
+      const fristImg = screen.findByTestId(cardZero);
+      const fristCardName = screen.findByTestId(recipeZero);
+      expect(fristImg).toBe('https://www.thecocktaildb.com/images/media/drink/vqyxqx1472669095.jpg');
+      expect(fristCardName).toBe('69 Special');
+    });
+  });
+  test('testa se ao pesquisar pelo componente Drinks por um ingrediente, o retorno da API é o correto!', async () => {
+    // Arrange
+    render(
+      <ApiProvider>
+        <HeaderProvider>
+          <Drinks />
+        </HeaderProvider>
+      </ApiProvider>,
+      { wrapper: BrowserRouter },
+    );
+    // Act
+    const searchButton = screen.getByTestId(search);
+    userEvent.click(searchButton);
+    const searchInput = screen.getByTestId(searchInputLint);
+    const searchButton2 = screen.getByTestId(execSearch);
+    const ingRa = screen.getByTestId(ingRadio);
+    userEvent.click(ingRa);
+    userEvent.type(searchInput, 'Limeade');
+    userEvent.click(searchButton2);
+    // Assert
+    waitFor(() => {
+      const fristImg = screen.findByTestId(cardZero);
+      const fristCardName = screen.findByTestId(recipeZero);
+      expect(fristImg).toBe('https://www.thecocktaildb.com/images/media/drink/xwxyux1441254243.jpg');
+      expect(fristCardName).toBe('Vodka Fizz');
+    });
+  });
+  test('testa se ao pesquisar por duas letras exibe um alert', async () => {
+    // Arrange
+    render(
+      <ApiProvider>
+        <HeaderProvider>
+          <Meals />
+        </HeaderProvider>
+      </ApiProvider>,
+      { wrapper: BrowserRouter },
+    );
+    // Act
+    const searchButton = screen.getByTestId(search);
+    userEvent.click(searchButton);
+    const searchInput = screen.getByTestId(searchInputLint);
+    const searchButton2 = screen.getByTestId(execSearch);
+    const firstLetter = screen.getByTestId(letter);
+    userEvent.click(firstLetter);
+    userEvent.type(searchInput, 'Limeade');
+    userEvent.click(searchButton2);
+    // Assert
+    waitFor(() => {
+      expect(global.alert).toBeCalledWith('Your search must have only 1 (one) character');
+    });
+  });
+  test('testa se ao pesquisar sem inserir nada no input search exibe um alert', async () => {
+    // Arrange
+    render(
+      <ApiProvider>
+        <HeaderProvider>
+          <Meals />
+        </HeaderProvider>
+      </ApiProvider>,
+      { wrapper: BrowserRouter },
+    );
+    // Act
+    const searchButton = screen.getByTestId(search);
+    userEvent.click(searchButton);
+    const searchButton2 = screen.getByTestId(execSearch);
+    userEvent.click(searchButton2);
+    // Assert
+    waitFor(() => {
+      expect(global.alert).toBeCalledWith('Search term not entered');
+    });
+  });
+  test('testa se ao pesquisar por algum termo que não existe receitas é exibido um alert', async () => {
+    // Arrange
+    render(
+      <ApiProvider>
+        <HeaderProvider>
+          <Meals />
+        </HeaderProvider>
+      </ApiProvider>,
+      { wrapper: BrowserRouter },
+    );
+    // Act
+    const searchButton = screen.getByTestId(search);
+    userEvent.click(searchButton);
+    const searchInput = screen.getByTestId(searchInputLint);
+    const searchButton2 = screen.getByTestId(execSearch);
+    const NameRadio = screen.getByTestId(nameRadio);
+    userEvent.click(NameRadio);
+    userEvent.type(searchInput, 'aaaaaa');
+    userEvent.click(searchButton2);
+    // Assert
+    waitFor(() => {
+      expect(global.alert).toBeCalledWith('Sorry, we haven\'t found any recipes for these filters.');
+    });
+  });
 });
- */
